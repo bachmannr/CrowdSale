03e46de73a9cf28f741126cf0137fbdaa7a54226
Ralph <ralph.bachmann@gmail.com>
2024-05-23 17:29:56 -0500

    multiple tokens

--- src/components/App.js
+++ src/components/App.js
@@ -46,7 +46,7 @@ function App() {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account)
-
+    console.log(account)
     // Fetch account balance
     const accountBalance = ethers.utils.formatUnits(await token.balanceOf(account), 18)
     setAccountBalance(accountBalance)
