1891ae3768a24464caab73a00b81ec3daa8db061
Ralph <ralph.bachmann@gmail.com>
2024-08-02 12:14:35 -0500

    crowdsale

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
+import "hardhat/console.sol";
 import "./Token.sol";
 contract Crowdsale {
     address public owner;
@@ -7,8 +8,10 @@ contract Crowdsale {
     uint256 public price;
     uint256 public maxTokens;
     uint256 public tokensSold;
+
     event Buy(uint256 amount, address buyer);
     event Finalize(uint256 tokensSold, uint256 ethRaised);
+
     constructor(Token _token, uint256 _price, uint256 _maxTokens) {
         owner = msg.sender;
         token = _token;
@@ -28,7 +31,9 @@ contract Crowdsale {
         //address of current smart contract address(this)
         require(token.balanceOf(address(this)) >= _amount);
         require(token.transfer(msg.sender, _amount), "Failed to transfer");
+
         tokensSold += _amount;
+        
         emit Buy(_amount, msg.sender);
     }
     function setPrice(uint256 _price) public onlyOwner {
